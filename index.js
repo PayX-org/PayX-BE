18b1bcde01270951e0be16349dec99eabbdf003a
royshohat <roy123123sohat@gmail.com>
2025-02-12 23:41:49 +0200

    set up lots of things including the tree and also mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const connectDB = require('./config/db');
 
 const app = express();
-
 const PORT = 3001;
+
+connectDB();
+
+// Middleware
+app.use(express.json());
+
 
 
 app.get("/", (req, res) => {
